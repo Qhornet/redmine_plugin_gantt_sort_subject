c610ca41072dd0c8ec26c778542942aed97318eb
mas0061 <mas0061@users.noreply.github.com>
2017-12-03 15:28:11 +0900

    add Plugin settings

--- lib/sort_subject_patch/gantt_patch.rb
+++ lib/sort_subject_patch/gantt_patch.rb
@@ -3,14 +3,22 @@ module GanttSortSubject
     extend ActiveSupport::Concern
 
     included do
+      original = Redmine::Helpers::Gantt.method(:sort_issue_logic)
+
       define_singleton_method :sort_issue_logic do |issue|
-        issue_subjects = []
-        current_issue = issue
-        begin
-          issue_subjects.unshift(current_issue.subject)
-          current_issue = current_issue.parent
-        end while (current_issue)
-        issue_subjects
+        # active
+        if '1' == Setting.plugin_redmine_plugin_gantt_sort_subject['sort_subject_enabled'].to_s
+          issue_subjects = []
+          current_issue = issue
+          begin
+            issue_subjects.unshift(current_issue.subject)
+            current_issue = current_issue.parent
+          end while (current_issue)
+          issue_subjects
+        # inactive -> original method
+        else
+          original.call(issue)
+        end
       end
     end
 
